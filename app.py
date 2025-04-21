d63dc7fbb3ec1609d16a652f108e7d1c87be13f2
drishtadyumna <github@drishtadyumna.com>
2025-04-21 12:15:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -347,7 +347,7 @@ st.markdown("---")
 # -------------------------------------------------------------------
 # UI: Chart Selection & Fetch
 # -------------------------------------------------------------------
-st.subheader("Select Charts to Fetch")
+st.subheader("Select Charts to Fetch - if you are getting any errors, try after 5 mins")
 c1, c2 = st.columns(2)
 if c1.button("Select All"):
     for k in CHART_ENDPOINTS: st.session_state[f"cb_{k}"] = True
